8841e414efd9c9c62d4ebb3561114d6cacd6c1c3
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-10 18:34:07 +0200

    updated task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const listEl = document.querySelector('.gallery')
+
+galleryItems.forEach(element => {
+ 
+    let markup = `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`
+    listEl.insertAdjacentHTML('beforeend',markup)
+})
+
+
+    const lightbox = new SimpleLightbox('.gallery a', { });
+
+
+
+
+
+
