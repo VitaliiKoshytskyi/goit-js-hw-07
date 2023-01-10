f2f201a6ebcb88ab4dfcc7a553e85b08eb691984
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-10 17:03:04 +0200

    updated 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const commonDivEl = document.querySelector('.gallery')
+
+
+
+galleryItems.forEach(element => {
+ 
+    let markup = `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`
+    commonDivEl.insertAdjacentHTML('beforeend',markup)
+})
+
+const gettingLargePictureHandler = (event) => {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+       return
+    } 
+     const url = event.target.dataset.source
+
+const instance = basicLightbox.create(`<img src="${url}" width="800" height="600">`)
+
+instance.show()
+}
+
+
+commonDivEl.addEventListener('click',gettingLargePictureHandler)
